73ca05475c147f3a16ae2723e1e3be33323445b5
Vivek-pen <vivek.penumutchu@gmail.com>
2026-04-01 09:12:47 +0530

    ML_Codes

--- week6.py
+++ week6.py
@@ -1,31 +1,40 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.datasets import load_iris
 
-data = load_iris()
-X = data.data
-y = data.target
+# FIXED DATA (same length)
+data = {
+    'age':    [23, 46, 64, 64, 21, 34, 45],
+    'income': [32000, 65000, 40000, 57000, 70000, 50000, 45000],
+    'buys':   [0, 1, 1, 1, 0, 0, 0]
+}
 
+df = pd.DataFrame(data)
+
+X = df[["age","income"]]
+y = df["buys"]
+
+# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
+# Model
 dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
 
-params = {
-    'max_depth': [2, 3, 4, 5],
-    'criterion': ['gini', 'entropy']
-}
+# Prediction
+y_pred = dt.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
 
-grid = GridSearchCV(dt, params, cv=5)
-grid.fit(X_train, y_train)
 
-print("Best Parameters:", grid.best_params_)
+plot_tree(dt, filled=True,
+          class_names=["No","Yes"],
+          feature_names=["Age", "Income"])
 
-best_model = grid.best_estimator_
-
-y_pred = best_model.predict(X_test)
-
-acc = accuracy_score(y_test, y_pred)
-print("Accuracy:", acc)
+plt.title("Decision Tree")
+plt.show()
